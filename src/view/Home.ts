adc4cd29a04181ffcbceddd8b40b9b2ae64043a2
dilrong <dilrong@dilrong.com>
2022-03-09 15:02:54 +0900

    fix: docs links

--- src/view/Home.ts
+++ src/view/Home.ts
@@ -124,7 +124,7 @@ export default class Landing implements View {
                             el("a.twitter", { href: "https://twitter.com/gaia_protocol", target: "_blank" },
                                 el("img", { src: "/images/shared/icn/icn-twitter.svg" }),
                             ),
-                            el("a.gitbook", { href: "https://docs.gaiasupernova.com/", target: "_blank" },
+                            el("a.gitbook", { href: "https://gaiaprotocol.notion.site/Gaia-Supernova-Docs-7f6a1ea8c1294c9ca5669481270b2e78", target: "_blank" },
                                 el("img", { src: "/images/shared/icn/icn-notion.svg" }),
                             ),
                         ),
